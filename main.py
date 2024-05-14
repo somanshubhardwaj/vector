f8b777f60e6ee64422c63b87c67849a1be6f5af2
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-05-14 11:04:39 +0530

    Add additional vector operations and special methods to Vector class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 class Vector:
+    
     def __init__(self,x,y,z):
         self.x = x
         self.y = y
@@ -48,5 +49,62 @@ class Vector:
         return Vector(math.ceil(self.x),math.ceil(self.y),math.ceil(self.z))
     def __trunc__(self):
         return Vector(math.trunc(self.x),math.trunc(self.y),math.trunc(self.z))
+    """
+    A class to represent a 3-dimensional vector and perform vector operations.
+
+    Attributes:
+    x (float): The x-coordinate of the vector.
+    y (float): The y-coordinate of the vector.
+    z (float): The z-coordinate of the vector.
+
+    Methods:
+    __init__(self, x, y, z):
+        Initializes a Vector object with the given x, y, and z coordinates.
+    __add__(self, other):
+        Adds two vectors element-wise and returns the resulting vector.
+    __sub__(self, other):
+        Subtracts one vector from another element-wise and returns the resulting vector.
+    __mul__(self, other):
+        Multiplies two vectors element-wise and returns the resulting vector.
+    __truediv__(self, other):
+        Divides one vector by another element-wise and returns the resulting vector.
+    __str__(self):
+        Returns a string representation of the vector in the format "Vector(x, y, z)".
+    scalar(self, other):
+        Computes the scalar (dot) product of two vectors and returns the result.
+    cross(self, other):
+        Computes the cross product of two vectors and returns the resulting vector.
+    __repr__(self):
+        Returns a string representation of the vector suitable for reproduction of the object.
+    __eq__(self, other):
+        Checks if two vectors are equal.
+    __ne__(self, other):
+        Checks if two vectors are not equal.
+    __lt__(self, other):
+        Checks if all components of self are less than corresponding components of other.
+    __le__(self, other):
+        Checks if all components of self are less than or equal to corresponding components of other.
+    __gt__(self, other):
+        Checks if all components of self are greater than corresponding components of other.
+    __ge__(self, other):
+        Checks if all components of self are greater than or equal to corresponding components of other.
+    __neg__(self):
+        Returns the negation of the vector.
+    __pos__(self):
+        Returns the vector unchanged.
+    __abs__(self):
+        Returns a vector with the absolute values of each component.
+    __invert__(self):
+        Returns a vector with bitwise inversion of each component.
+    __round__(self, n):
+        Returns a vector with each component rounded to the nearest multiple of 10**(-n).
+    __floor__(self):
+        Returns a vector with each component rounded down to the nearest integer.
+    __ceil__(self):
+        Returns a vector with each component rounded up to the nearest integer.
+    __trunc__(self):
+        Returns a vector with each component truncated to the nearest integer towards zero.
+    """
+
 
  
